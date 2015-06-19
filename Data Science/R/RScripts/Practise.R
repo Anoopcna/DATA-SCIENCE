2b5fab2ed251b3d944a2f3f243a68351bd44b95f
Anoop Nair <anoopcna@gmail.com>
2015-06-19 13:42:42 +0530

    Added data Science Folder

---
+++ Data Science/R/RScripts/Practise.R
@@ -0,0 +1,51 @@
+# loop Try
+# a=2
+# while(a<10)
+# {
+#   print(a)
+#   a=a+1
+# }
+#CTRL+SHift+C--to comment multiple selected lines in R.
+#CTRL+l to clear the Screen
+#ls()to get the list of variables
+#typeof(a)to get the type of a variable.
+#rm()to remove a variable from the memory.
+#?[function_name]help for the function name.
+#length(a)the length of the vector.
+#arr[2:4]meaning from the second element to 4th element.
+#seq(0,20,5)to generate a sequence.
+
+
+#two things are performed using packages
+#1.INstalling the package--install.packages("packagename")
+#2.Loading the package.---library("Boot")
+#setwd()-for changin the directory on which R is gona work.
+#getwd()-to check currently on which directory we are working.
+#dir()to get the contents from the directory.
+#read.table()to put the data into the table from an excel sheet.
+#class(x)gives out the type nof object.
+#nrow()-number of rows
+#ncol()number of columns.
+#summary()-gives the basic descriptive statistics for each variable in a data fram
+#like the mean,Median,1st quartile,2nd quartile,3rd quartile,min,max etc
+
+#scan()-you can dynamically enter the values here using this function.
+#names()-to find out the names of the columns.
+#plot functions-Strip chart-stripchart(column name)
+#histogram-hist(column name)
+#boxplot-boxplot(column name)
+#rep() replicate a value.
+#order()-to sort the function.
+#cbind()-to combine the whole as a column
+#rbind()-to combine the whole as a row.
+#find.package("pacakage name")-to check if the package is ther on our system.
+# > library(stats) 
+# > search() -to search for the functions inside a packages.
+#find_rtools()-to check if r tools is properly installed in the system:
+#should return True.
+#Kmeans(numerical data,number of clusters,no. of iteration)
+
+
+
+
+
